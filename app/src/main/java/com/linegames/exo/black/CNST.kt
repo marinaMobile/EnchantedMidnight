d5c2d109a7dc32a4e5ba332024c953d3652fded4
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 13:35:58 +0300

    first commit

---
+++ app/src/main/java/com/linegames/exo/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.linegames.exo.black
+
+object CNST {
+    const val AF_DEV_KEY = "yns3LrqvSKZiyDYMq5r5Rc"
+    const val jsoupCheck = "3g5d"
+    const val ONESIGNAL_APP_ID = "ae563025-6806-49bc-9238-f41e3728dace"
+
+    var lru = "http://enchantedmidnight.xyz/go.php?to=1&"
+
+
+    val odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
